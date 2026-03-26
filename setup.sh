7fb1e1400348a8d66c4833d413b31ba72033fc84
jdmartin <jonsaccount+githubjd@icloud.com>
2026-03-25 22:14:31 -0700

    use sudo

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 install-required-software () {
-    apt-get update;
-    apt-get install \
+    sudo apt-get update;
+    sudo apt-get install \
         needrestart \
         samba \
         smbclient \
@@ -12,62 +12,62 @@ install-required-software () {
 }
 
 setup-unattended-uprades () {
-    mkdir -p /etc/apt/apt.conf.d;
-    cp ./src/etc/apt/apt.conf.d/52unattended-upgrades-local /etc/apt/apt.conf.d/
+    sudo mkdir -p /etc/apt/apt.conf.d;
+    sudo cp ./src/etc/apt/apt.conf.d/52unattended-upgrades-local /etc/apt/apt.conf.d/
 }
 
 setup-udev-rules () {
-    mkdir -p /etc/udev/rules.d;
-    cp ./src/etc/udev/rules.d/10-write-blocker.rules /etc/udev/rules.d/;
-    systemctl stop udisks2;
-    systemctl mask udisks2;
+    sudo mkdir -p /etc/udev/rules.d;
+    sudo cp ./src/etc/udev/rules.d/10-write-blocker.rules /etc/udev/rules.d/;
+    sudo systemctl stop udisks2;
+    sudo systemctl mask udisks2;
 }
 
 setup-dconf-locks () {
-    mkdir -p /etc/dconf/db/local.d/locks;
-    cp ./src/etc/dconf/db/local.d/00-disable-automount /etc/dconf/db/local.d/00-disable-automount;
-    cp ./src/etc/dconf/db/local.d/locks/automount /etc/dconf/db/local.d/locks/automount;
-    dconf update
+    sudo mkdir -p /etc/dconf/db/local.d/locks;
+    sudo cp ./src/etc/dconf/db/local.d/00-disable-automount /etc/dconf/db/local.d/00-disable-automount;
+    sudo cp ./src/etc/dconf/db/local.d/locks/automount /etc/dconf/db/local.d/locks/automount;
+    sudo dconf update;
 }
 
 setup-auto-ingest-script () {
-    mkdir -p /usr/local/bin;
-    mkdir -p /etc/systemd/system/systemd-udevd.service.d;
-    cp ./src/usr/local/bin/auto-ingest.sh /usr/local/bin/auto-ingest.sh;
-    cp ./src/etc/systemd/system/auto-ingest@.service /etc/systemd/system/auto-ingest@.service;
-    cp ./src/etc/systemd/system/systemd-udevd.service.d/override.conf /etc/systemd/system/systemd-udevd.service.d/override.conf
-    chmod +x /usr/local/bin/auto-ingest.sh;
-    systemctl daemon-reload;
-    systemctl enable --now auto-ingest@.service;
-    systemctl restart systemd-udevd;
-    udevadm control --reload-rules
+    sudo mkdir -p /usr/local/bin;
+    sudo mkdir -p /etc/systemd/system/systemd-udevd.service.d;
+    sudo cp ./src/usr/local/bin/auto-ingest.sh /usr/local/bin/auto-ingest.sh;
+    sudo cp ./src/etc/systemd/system/auto-ingest@.service /etc/systemd/system/auto-ingest@.service;
+    sudo cp ./src/etc/systemd/system/systemd-udevd.service.d/override.conf /etc/systemd/system/systemd-udevd.service.d/override.conf
+    sudo chmod +x /usr/local/bin/auto-ingest.sh;
+    sudo systemctl daemon-reload;
+    sudo systemctl enable --now auto-ingest@.service;
+    sudo systemctl restart systemd-udevd;
+    sudo udevadm control --reload-rules;
 }
 
 setup-samba-share () {
-    mkdir -p /etc/samba;
-    cat ./src/etc/samba/smb.conf.local >> /etc/samba/smb.conf;
-    systemctl restart samba;
+    sudo mkdir -p /etc/samba;
+    sudo cat ./src/etc/samba/smb.conf.local >> /etc/samba/smb.conf;
+    sudo systemctl restart samba;
 }
 
 setup-web-control () {
-    mkdir -p /var/www/control;
-    cp ./src/var/www/control/app.py /var/www/control/;
-    cp ./src/etc/systemd/system/web-control.service /etc/systemd/system/web-control.service;
-    systemctl daemon-reload;
-    systemctl enable --now web-control.service;
+    sudo mkdir -p /var/www/control;
+    sudo cp ./src/var/www/control/app.py /var/www/control/;
+    sudo cp ./src/etc/systemd/system/web-control.service /etc/systemd/system/web-control.service;
+    sudo systemctl daemon-reload;
+    sudo systemctl enable --now web-control.service;
 }
 
 setup-local-only-network () {
-    nmcli connection add type ethernet \
+    sudo nmcli connection add type ethernet \
         con-name Forensic-Net \
         ifname eth0 \
         ipv4.method shared \
         ipv4.addresses 192.168.99.50/24;
 
-    nmcli connection modify Forensic-Net \
+    sudo nmcli connection modify Forensic-Net \
         connection.autoconnect yes;
 
-    nmcli connection up Forensic-Net;
+    sudo nmcli connection up Forensic-Net;
 }
 
 install-required-software;
